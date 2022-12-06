ec6d8c3ea99a451a3c2eecbc7f2c2803c0c38296
Guigghisi <guilhermeghisi14@gmail.com>
2022-12-06 18:14:09 -0300

    M2S08E6

--- FirstProject/src/main/java/tech/devinhouse/firstproject/controller/PessoaController.java
+++ FirstProject/src/main/java/tech/devinhouse/firstproject/controller/PessoaController.java
@@ -2,28 +2,31 @@ package tech.devinhouse.firstproject.controller;
 
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.HttpStatusCodeException;
+import tech.devinhouse.firstproject.model.PessoaModel;
 import tech.devinhouse.firstproject.service.PessoaService;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/pessoas")
 public class PessoaController {
     private PessoaService service;
-    
+
     public PessoaController(PessoaService service){
         this.service = service;
     }
 
 
     @GetMapping()
-    public String getPessoas(){
-        return "teste";
+    public List<PessoaModel> getPessoas(){
+        if(service.getPessoas().isEmpty()){
+            return null;
+        }
+        return service.getPessoas();
     }
-    @GetMapping("/{id}")
-    public String getPessoasById(@PathVariable("id") String id){
-        return id;
-    }
+
 
 }

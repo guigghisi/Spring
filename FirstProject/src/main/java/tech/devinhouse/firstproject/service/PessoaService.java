c2b9d656009021123408d825166e9c686c84e325
Guigghisi <guilhermeghisi14@gmail.com>
2022-12-06 18:14:09 -0300

    M2S08E6

--- FirstProject/src/main/java/tech/devinhouse/firstproject/service/PessoaService.java
+++ FirstProject/src/main/java/tech/devinhouse/firstproject/service/PessoaService.java
@@ -2,14 +2,28 @@ package tech.devinhouse.firstproject.service;
 
 
 import org.springframework.stereotype.Service;
+import tech.devinhouse.firstproject.model.PessoaModel;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class PessoaService {
 
+    private  static List<PessoaModel> pessoaModelList = new ArrayList<PessoaModel>();
     public PessoaService() {
+        PessoaModel pessoa = new PessoaModel(1L,"Pessoa 1");
+        PessoaModel pessoa2 = new PessoaModel(2L,"Pessoa 2");
+        pessoaModelList.add(pessoa);
+        pessoaModelList.add(pessoa2);
     }
 
-    public void getPessoas(){}
+    public List<PessoaModel> getPessoas(){
+        if(pessoaModelList.isEmpty()){
+            return null;
+        }
+        return pessoaModelList;
+    }
     public void getPessoa(){}
     public void postPessoa(){}
     public void updatePessoa(){}
